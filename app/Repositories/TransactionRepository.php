38d64d392b58d8ff115a35eadd35e03c8665c2d5
Mahen <mahendra1d17@gmail.com>
2026-04-15 15:55:57 +0700

    transaction repo

---
+++ app/Repositories/TransactionRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Repositories;
+use App\Models\Transaction;
+use App\Models\TransactionProduct;
+
+
+class TransactionRepository
+{
+    public function getAll(array $fields)
+    {
+        return Transaction::select($fields)
+        ->with(['TransactionProducts.Product','Merchant.keeper'])
+        ->latest()
+        ->paginate(10);
+    }
+
+    public function getById($id, array $fields)
+    {
+        return Transaction::select($fields)
+        ->with(['TransactionProducts.Product','Merchant.keeper'])
+        ->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+      return Transaction::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $transaction = Transaction::findOrFail($id);
+        $transaction->update($data);
+        return $transaction;
+    }
+
+    public function delete($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+        $transaction->delete();
+    }
+
+    public function createTransactionProducts($transactionId, array $products)
+    {
+        foreach ($products as $product) {
+        $subtotal = $product['quantity'] * $product['price'];
+
+
+            TransactionProduct::create([
+                'transaction_id' => $transactionId,
+                'product_id' => $product['product_id'],
+                'quantity' => $product['quantity'],
+                'price' => $product['price'],
+                'subtotal' => $subtotal,
+            ]);
+        }
+    }
+
+    public function getTransactionByMerchantId($merchantId)
+    {
+        return Transaction::where('merchant_id', $merchantId)
+        ->select(['id','name','phone','merchant_id', 'grand_total', 'created_at'])
+        ->with(['TransactionProducts.Product','Merchant.keeper'])
+        ->get();
+    }   
+
+}
